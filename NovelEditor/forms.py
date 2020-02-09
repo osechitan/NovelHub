b19ec3bae1ba2eb87b53fda4d6b9ed5fad1ab7e4
osechitan <toshine212@hotmail.co.jp>
2020-02-10 07:10:02 +0900

    add updateview

--- NovelEditor/forms.py
+++ NovelEditor/forms.py
@@ -3,6 +3,7 @@ from django.core.mail import EmailMessage
 
 
 from .models import Novel
+from .models import NovelHistory
 
 
 class NovelCreateForm(forms.ModelForm):
@@ -16,8 +17,10 @@ class NovelCreateForm(forms.ModelForm):
 
 class NovelUpdateForm(forms.ModelForm):
     class Meta:
-        model = Novel
-        fields = ('title', 'body',)
+        model = NovelHistory
+        fields = ('body',)
 
     def __init__(self, *args, **kwargs):
+        print('formのprintarg={}'.format(args))
+        print('formのprintkwargs={}'.format(kwargs))
         super().__init__(*args, **kwargs)
--- NovelEditor/views.py
+++ NovelEditor/views.py
@@ -66,6 +66,60 @@ class NovelCreateView(LoginRequiredMixin, generic.CreateView):
         return super().form_invalid(form)
 
 
+class NovelUpdateView(LoginRequiredMixin, generic.CreateView):
+    model = NovelHistory
+    template_name = 'novel_update.html'
+    form_class = NovelUpdateForm
+
+    def get_initial(self):
+        novel = Novel.objects.filter(id=self.kwargs['pk'])
+        initial = super().get_initial()
+        # initial["title"] = novel[0].title
+        initial["body"] = novel[0].body
+        return initial
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'history': NovelHistory.objects.filter(novel_id=self.kwargs['pk'])
+        })
+        return context
+
+    def form_valid(self, form):
+
+        # version = Novel.objects.filter(user=self.request.user).aggregate(latest_version=Max('version'))
+        novel_history = form.save(commit=False)
+        novel = Novel.objects.get(id=self.kwargs['pk'])
+        novel_history.novel_id = novel
+        novel_history.created_at = timezone.now()
+        novel_history.updated_at = timezone.now()
+        # novel.version = version['latest_version'] + 1
+        novel_history.save()
+        # messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('NovelHub:novel_detail', kwargs={'pk': self.kwargs['pk']})
+
+
+"""
+class NovelUpdateView(LoginRequiredMixin, generic.FormView):
+    model = Novel
+    template_name = 'novel_update.html'
+    form_class = NovelUpdateForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'history': NovelHistory.objects.filter(novel_id=self.kwargs['pk'])
+        })
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('NovelHub:novel_detail', kwargs={'pk': self.kwargs['pk']})
+"""
+
+"""
 class NovelUpdateView(LoginRequiredMixin, generic.TemplateView):
     model = Novel
     template_name = 'novel_update.html'
@@ -74,9 +128,9 @@ class NovelUpdateView(LoginRequiredMixin, generic.TemplateView):
     def get(self, request, *args, **kwargs):
         form = NovelUpdateForm(request.POST)
         form.is_valid()
-        print('self={}'.format(kwargs['pk']))
         history = NovelHistory.objects.filter(novel_id=kwargs['pk'])
         return render(request, 'novel_update.html', {'form': form, 'history': history})
 
     def get_success_url(self):
         return reverse_lazy('NovelHub:novel_detail', kwargs={'pk': self.kwargs['pk']})
+"""
