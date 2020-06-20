bc955feac3fd880f0405ca35ce69048f59d4e577
osechitan <toshine212@hotmail.co.jp>
2020-06-20 19:16:47 +0900

    NovelPost のadmin.py修正

--- NovelPost/admin.py
+++ NovelPost/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import NovelPost
+
+class NovelPostModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at')
+    ordering = ('created_at',)
+
+admin.site.register(NovelPost, NovelPostModelAdmin)
